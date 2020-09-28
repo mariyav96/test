41319a5be9a9ba51fc3f97f7ed7fa1b065a6072b
Mariya <mvarghe4@asu.edu>
2020-09-27 22:27:07 -0900

    retest

---
+++ Unity_demo_game - Copy/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject WinnerText;
+    float xInput;
+    float zInput;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject, 3f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            SceneManager.LoadScene("NewLevel");
+        }
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+        rb.AddForce(xInput * speed, 0, zInput * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            WinnerText.SetActive(true);
+        }
+    }
+}
